8c98f2eb135a01154f56b5b559279826d2484e8a
chlee <1chlee1@naver.com>
2023-03-25 17:15:16 +0900

    1

---
+++ src/main/java/com/server/rds_server/RdsServerApplication.java
@@ -0,0 +1,13 @@
+package com.server.rds_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RdsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RdsServerApplication.class, args);
+    }
+
+}
